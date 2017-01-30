bdf883ed6e619e45dd121135ed3c46b3a9a05847
ArginFiorenza <arginfiorenza25121@gmail.com>
2017-01-30 13:55:09 +0700

    Add files via upload

---
+++ CJS11.js
@@ -0,0 +1,3 @@
+/*<![CDATA[*/
+var perPage=10,numPages=5,firstText="<i class='fa fa-caret-left'></i><i class='fa fa-caret-left'></i> First",lastText="Last <i class='fa fa-caret-right'></i><i class='fa fa-caret-right'></i>",prevText="<i class='fa fa-caret-left'></i> Prev",nextText="Next <i class='fa fa-caret-right'></i>",urlactivepage=location.href,home_page="/";
+/*]]>*/
