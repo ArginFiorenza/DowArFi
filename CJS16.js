f407fb2f35bde3f2f79c5a85b831eebacbb9d468
ArginFiorenza <arginfiorenza25121@gmail.com>
2017-01-30 14:01:49 +0700

    Add files via upload

---
+++ CJS16.js
@@ -0,0 +1,1 @@
+window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src=&quot;https://platform.twitter.com/widgets.js&quot;;fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,&quot;script&quot;,&quot;twitter-wjs&quot;));
